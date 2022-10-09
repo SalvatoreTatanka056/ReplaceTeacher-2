1eaec5ad9ed1be1b2fd63258399c930debf97e8d
app-tate-a-tate <tatanka056@gmail.com>
2022-10-09 11:40:11 +0200

    Add files via upload

--- WinFormsApp1/frmMain.Designer.cs
+++ WinFormsApp1/frmMain.Designer.cs
@@ -147,7 +147,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(149, 30);
             this.button2.TabIndex = 33;
-            this.button2.Text = "&Stampa";
+            this.button2.Text = "&Export ";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
