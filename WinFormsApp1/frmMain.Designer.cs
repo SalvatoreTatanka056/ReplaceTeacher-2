3105eb96cf7f33d734d4b38fa0f7944d2db15a34
Salvatore Romano <tatanka056@outlook.it>
2022-10-09 14:52:36 +0200

    modifica layout

--- WinFormsApp1/frmMain.Designer.cs
+++ WinFormsApp1/frmMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.panel1 = new System.Windows.Forms.Panel();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.button1 = new System.Windows.Forms.Button();
@@ -36,7 +37,6 @@
             this.splitContainer3 = new System.Windows.Forms.SplitContainer();
             this.splitContainer4 = new System.Windows.Forms.SplitContainer();
             this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
@@ -95,7 +95,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(1250, 6);
+            this.button4.Location = new System.Drawing.Point(1262, 6);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(106, 30);
             this.button4.TabIndex = 37;
@@ -136,7 +136,7 @@
             // 
             this.splitContainer3.Panel2.Controls.Add(this.dataGridView2);
             this.splitContainer3.Size = new System.Drawing.Size(1368, 299);
-            this.splitContainer3.SplitterDistance = 114;
+            this.splitContainer3.SplitterDistance = 120;
             this.splitContainer3.TabIndex = 22;
             this.splitContainer3.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer3_SplitterMoved);
             // 
@@ -147,47 +147,45 @@
             this.splitContainer4.Location = new System.Drawing.Point(0, 0);
             this.splitContainer4.Name = "splitContainer4";
             // 
+            // splitContainer4.Panel1
+            // 
+            this.splitContainer4.Panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("splitContainer4.Panel1.BackgroundImage")));
+            this.splitContainer4.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            // 
             // splitContainer4.Panel2
             // 
             this.splitContainer4.Panel2.Controls.Add(this.button2);
-            this.splitContainer4.Panel2.Controls.Add(this.label4);
             this.splitContainer4.Panel2.Controls.Add(this.dateTimePicker1);
             this.splitContainer4.Panel2.Controls.Add(this.button3);
-            this.splitContainer4.Size = new System.Drawing.Size(1368, 114);
-            this.splitContainer4.SplitterDistance = 729;
+            this.splitContainer4.Size = new System.Drawing.Size(1368, 120);
+            this.splitContainer4.SplitterDistance = 309;
             this.splitContainer4.TabIndex = 32;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(246, 12);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 51);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 30);
+            this.button2.Size = new System.Drawing.Size(1053, 26);
             this.button2.TabIndex = 33;
             this.button2.Text = "&Export ";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(331, 75);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 15);
-            this.label4.TabIndex = 35;
-            this.label4.Text = "Scelta data";
-            // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(412, 67);
+            this.dateTimePicker1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dateTimePicker1.Location = new System.Drawing.Point(0, 28);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.Size = new System.Drawing.Size(1053, 23);
             this.dateTimePicker1.TabIndex = 34;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(412, 12);
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 0);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(200, 30);
+            this.button3.Size = new System.Drawing.Size(1053, 28);
             this.button3.TabIndex = 32;
             this.button3.Text = "&Calcola Sostituzioni";
             this.button3.UseVisualStyleBackColor = true;
@@ -202,7 +200,7 @@
             this.dataGridView2.Location = new System.Drawing.Point(0, 0);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowTemplate.Height = 25;
-            this.dataGridView2.Size = new System.Drawing.Size(1368, 181);
+            this.dataGridView2.Size = new System.Drawing.Size(1368, 175);
             this.dataGridView2.TabIndex = 22;
             this.dataGridView2.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView2_CellFormatting);
             // 
@@ -265,7 +263,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
             this.splitContainer3.ResumeLayout(false);
             this.splitContainer4.Panel2.ResumeLayout(false);
-            this.splitContainer4.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).EndInit();
             this.splitContainer4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
@@ -288,7 +285,6 @@
         private SplitContainer splitContainer3;
         private SplitContainer splitContainer4;
         private Button button2;
-        private Label label4;
         private DateTimePicker dateTimePicker1;
         private Button button3;
         private DataGridView dataGridView2;
